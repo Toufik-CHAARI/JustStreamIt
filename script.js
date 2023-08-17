07de1a9588d2238e8c8d4378fac9f147bc29c7a3
Toufik-CHAARI <Toufik-CHAARI>
2023-08-17 16:53:09 +0200

    addedimprovement of page layout + implementation of the modal window logic and content

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const showModal = (movie) => {
+  const modal = document.getElementById('myModal');
+  document.getElementById('modal-image').src = movie.image_url;
+  document.getElementById('modal-title').innerText = `Titre: ${movie.title}`;
+  document.getElementById('modal-genres').innerText = movie.genres.join(', ');
+  document.getElementById('modal-year').innerText = `Date de sortie : ${movie.year}`;
+  document.getElementById('modal-imdb').innerText = `IMDb Score : ${movie.imdb_score}`;
+  document.getElementById('modal-votes').innerText = `Votes : ${movie.votes}`;
+  document.getElementById('modal-directors').innerText = `Directors: ${movie.directors.join(', ')}`;
+  document.getElementById('modal-actors').innerText = `Acteurs: ${movie.actors.join(', ')}`;
+  document.getElementById('modal-writers').innerText = `Scénario: ${movie.writers.join(', ')}`;
+  modal.style.display = "block";
+};
+
 const displayMovies = (movies, elementId, includeSummary = false) => {
   const container = document.getElementById(elementId);
   movies.forEach(movie => {
@@ -8,27 +22,29 @@ const displayMovies = (movies, elementId, includeSummary = false) => {
       <div class="${includeSummary ? 'text-block' : ''}">
         <h3>${movie.title}</h3>
         ${includeSummary ? 
-          `<p class="summary">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même.</p><button>Click Me</button>` : 
+          `<p id="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><button>Play</button>` : 
           `<p>IMDb Score: ${movie.imdb_score}</p>`}
       </div>
     `;
+    movieDiv.querySelector('img').addEventListener('click', () => showModal(movie));
+    if (includeSummary) {
+      movieDiv.querySelector('button').addEventListener('click', () => showModal(movie));
+    }
     container.appendChild(movieDiv);
   });
 };
-
 
 const fetchTopOverallMovie = async () => {
   const url = `http://localhost:8000/api/v1/titles/?sort_by=-imdb_score`;
   const response = await fetch(url);
   const data = await response.json();
-  const topMovie = data.results[0]; // Le film le mieux noté
+  const topMovie = data.results[0];
   displayMovies([topMovie], 'top-movie-overall', true);
 };
 
 const fetchTopMoviesByGenre = async (genre, elementId) => {
   let url = `http://localhost:8000/api/v1/titles/?genre=${genre}&sort_by=-imdb_score`;
   const allMovies = [];
-
   while (allMovies.length < 15) {
     const response = await fetch(url);
     const data = await response.json();
@@ -36,7 +52,6 @@ const fetchTopMoviesByGenre = async (genre, elementId) => {
     if (data.next) url = data.next;
     else break;
   }
-
   const topMovies = allMovies.slice(0, 15);
   displayMovies(topMovies, elementId);
 };
@@ -44,7 +59,6 @@ const fetchTopMoviesByGenre = async (genre, elementId) => {
 const fetchTop7OverallMovies = async () => {
   let url = `http://localhost:8000/api/v1/titles/?sort_by=-imdb_score`;
   const top7Movies = [];
-  
   while (top7Movies.length < 15) {
     const response = await fetch(url);
     const data = await response.json();
@@ -52,18 +66,34 @@ const fetchTop7OverallMovies = async () => {
     if (data.next) url = data.next;
     else break;
   }
-  
   const topMovies2 = top7Movies.slice(0, 15);
-
-  displayMovies(top7Movies2, 'top-7-movies-overall');
+  displayMovies(topMovies2, 'top-7-movies-overall');
 };
 
+document.getElementById('modal-close-button').addEventListener('click', () => {
+  document.getElementById('myModal').style.display = 'none';
+});
 
+
+function scrollToLeft(elementId) {
+  var element = document.getElementById(elementId);
+  element.scrollLeft -= 20; // adjust the scroll amount
+}
+
+function scrollToRight(elementId) {
+  const container = document.getElementById(elementId);
+  container.scrollLeft += 20; //  adjust the scroll amount
+}
 
 
 
 fetchTopOverallMovie();
 fetchTop7OverallMovies();
-fetchTopMoviesByGenre('Action', 'action-movie-container');
-fetchTopMoviesByGenre('Adventure', 'adventure-movie-container');
-fetchTopMoviesByGenre('Animation', 'animation-movie-container');
+fetchTopMoviesByGenre('Sci-Fi', 'action-movie-container');
+fetchTopMoviesByGenre('Fantasy', 'adventure-movie-container');
+fetchTopMoviesByGenre('Family', 'animation-movie-container');
+console.log("Hello World");
+
+
+
+
